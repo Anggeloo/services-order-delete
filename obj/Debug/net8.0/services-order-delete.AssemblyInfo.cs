8c9f7ecfb8f3c866224d177b83fd896adf1d6843
Anggelo <anstalyn1999@gmail.com>
2025-02-12 00:32:03 -0500

    feat: :rocket: Update the archive deploy.yml

--- obj/Debug/net8.0/services-order-delete.AssemblyInfo.cs
+++ obj/Debug/net8.0/services-order-delete.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("services-order-delete")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2f602ca4ef7cd8ea012f90728308a868bb9b3f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a12ce4a17e2f13b26b1072f3c2e9cd89c66f5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("services-order-delete")]
 [assembly: System.Reflection.AssemblyTitleAttribute("services-order-delete")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
